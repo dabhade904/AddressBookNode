5cf9ff5cfb19ce67a0e857ac529ac58fb4cfc154
dabhade904 <dabhade904@gmail.com>
2022-09-15 14:35:40 +0530

    Added : functionality to create address book attributes

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,26 @@
-console.log("welcome to address book problem");
+class AddressBook{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+    constructor(...params){
+        this.firstName=params[0];
+        this.lastName=params[1];
+        this.address=params[2];
+        this.city=params[3];
+        this.state=params[4];
+        this.zip=params[5];
+        this.phoneNumber=params[6];
+        this.email=params[7];
+    }
+    toString(){
+        return " Name : "+this.firstName+" "+this.lastName+" Address "+this.address+" "+this.city+" "+this.state+" "+this.zip+" Phone Number "+" Email "+this.email;
+    }
+}
+ console.log("welcome");
+ let address= new AddressBook("sachin","dabhade","shekta","bidkeen","mh",431105,93097339906,"dabahde904@gmail");
+ console.log("Address are : "+address);
